2bb5f0dc925927508784b6801e89d1a113fed3af
Morgoth228 <Dogman113636@gmail.com>
2023-05-07 10:20:45 +0300

    preliminary implementation of json factory

--- lib/entity/chirk.dart
+++ lib/entity/chirk.dart
@@ -18,4 +18,26 @@ class Chirk {
       this.likeCount = 0,
       this.disLikeCount = 0,
       this.liked});
+
+  factory Chirk.fromJson(Map<String, dynamic> json) {
+    return Chirk(
+      id: json['id'] as int,
+      dateTime: DateTime.parse(json['dateTime'] as String),
+      text: json['text'] as String,
+      user: User.fromJson(json['user'] as Map<String, dynamic>),
+      likeCount: json['likeCount'] as int,
+      disLikeCount: json['disLikeCount'] as int,
+      liked: json['liked'] as bool,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'dateTime': dateTime.toIso8601String(),
+    'text': text,
+    'user': user.toJson(),
+    'likeCount': likeCount,
+    'disLikeCount': disLikeCount,
+    'liked': liked,
+  };
 }
--- lib/entity/user.dart
+++ lib/entity/user.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 class User {
   final int id;
   final String login;
@@ -14,4 +15,24 @@ class User {
     required this.name,
     required this.surname,
   });
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'] as int,
+      login: json['login'] as String,
+      password: json['password'] as String,
+      iconId: json['iconId'] as int,
+      name: json['name'] as String,
+      surname: json['surname'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'login': login,
+    'password': password,
+    'iconId': iconId,
+    'firstName': name,
+    'lastName': surname,
+  };
+
 }
