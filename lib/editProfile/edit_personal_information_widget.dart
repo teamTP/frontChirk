3c4f9dd16094c8d251e1874e7cad643cca439290
Morgoth228 <Dogman113636@gmail.com>
2023-06-02 21:12:34 +0300

    create validator for personal information

--- lib/editProfile/edit_personal_information_widget.dart
+++ lib/editProfile/edit_personal_information_widget.dart
@@ -16,35 +16,47 @@ class EditPersonalInformationWidget
           icon: const Icon(Icons.clear),
         ),
         actions: <Widget>[
-          IconButton(onPressed: () => wm.editInf(), icon: const Icon(Icons.done)),
+          IconButton(
+              onPressed: () => wm.editInf(), icon: const Icon(Icons.done)),
         ],
       ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(32),
-          child: Form(
-            key: wm.key,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 16),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: "Имя",
-                    border: OutlineInputBorder(),
-                  ),
-                  controller: wm.nameController,
+          child: EntityStateNotifierBuilder(
+            listenableEntityState: wm.userState,
+            builder: (context, user) {
+              return Form(
+                key: wm.key,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Имя",
+                        errorText: wm.isFirstNameValid
+                            ? null
+                            : "Поле не должно быть пустым",
+                        border: OutlineInputBorder(),
+                      ),
+                      controller: wm.nameController,
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        errorText: wm.isLastNameValid
+                            ? null
+                            : "Поле не должно быть пустым",
+                        labelText: "Фамилия",
+                        border: OutlineInputBorder(),
+                      ),
+                      controller: wm.surnameController,
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: "Фамилия",
-                    border: OutlineInputBorder(),
-                  ),
-                  controller: wm.surnameController,
-                ),
-              ],
-            ),
+              );
+            },
           ),
         ),
       ),
--- lib/editProfile/edit_personal_information_wm.dart
+++ lib/editProfile/edit_personal_information_wm.dart
@@ -2,22 +2,31 @@ import 'package:chirk/editProfile/edit_personal_information_widget.dart';
 import 'package:elementary/elementary.dart';
 import 'package:flutter/cupertino.dart';
 
+import '../entity/user.dart';
 import 'edit_personal_information_model.dart';
 class EditPersonalInformationWM extends WidgetModel<EditPersonalInformationWidget, EditPersonalInformationModel> implements IEditPersonalInformationWM{
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _surnameController = TextEditingController();
   final Key _key = GlobalKey<FormState>();
+  bool _isFirstNameValid = true;
+  bool _isLastNameValid = true;
+  EntityStateNotifier<User> _userState = EntityStateNotifier();
+
   EditPersonalInformationWM(super.model){
     _nameController.text = model.oldUser.name;
     _surnameController.text = model.oldUser.surname;
+    _userState.content(model.oldUser);
   }
   @override
   void editInf() {
-    model.editInf(_nameController.text,  _surnameController.text).then((value){
-      model.oldUser.name = _nameController.text;
-      model.oldUser.surname = _surnameController.text;
-      Navigator.pop(context, model.oldUser);
-    });
+    if(_validateName()&& _validateSurname()){
+      model.editInf(_nameController.text,  _surnameController.text).then((value){
+        model.oldUser.name = _nameController.text;
+        model.oldUser.surname = _surnameController.text;
+        Navigator.pop(context, model.oldUser);
+      });
+    }
+
   }
 
   @override
@@ -34,11 +43,35 @@ class EditPersonalInformationWM extends WidgetModel<EditPersonalInformationWidge
   @override
   Key get key => _key;
 
+  bool _validateName() {
+    bool isValid = _nameController.text.isNotEmpty;
+    _isFirstNameValid = isValid;
+    _userState.content(model.oldUser);
+    return isValid;
+  }
+  bool _validateSurname() {
+    bool isValid = _surnameController.text.isNotEmpty;
+    _isLastNameValid = isValid;
+    _userState.content(model.oldUser);
+    return isValid;
+  }
+
+  @override
+  EntityStateNotifier<User> get userState => _userState;
+
+  @override
+  bool get isFirstNameValid =>_isFirstNameValid;
+
+  @override
+  bool get isLastNameValid => _isLastNameValid;
 
 }
 abstract class IEditPersonalInformationWM extends IWidgetModel{
   TextEditingController get nameController;
   TextEditingController get surnameController;
+  EntityStateNotifier<User> get userState;
+  bool get isFirstNameValid;
+  bool get isLastNameValid;
 
   Key get key;
   void editInf();
